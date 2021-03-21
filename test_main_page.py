81e4513686b429139609b06899cdad903bdac7bb
cocolacre <cocolacre@gmail.com>
2021-03-22 00:18:08 +0300

    Login page locators

--- test_main_page.py
+++ test_main_page.py
@@ -6,8 +6,8 @@ from pages.main_page import MainPage
 #    login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
+    link ="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     link = "http://selenium1py.pythonanywhere.com/"
-    
     #browser.get(link)
     #login_link = browser.find_element_by_css_selector("#login_link")
     #login_link.click()
@@ -18,7 +18,9 @@ def test_guest_can_go_to_login_page(browser):
     
     
 def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     link = "http://selenium1py.pythonanywhere.com/"
+    
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
