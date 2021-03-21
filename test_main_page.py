03c9cd5d3ddfc790fac92a60beedacd8619bdb7f
cocolacre <cocolacre@gmail.com>
2021-03-21 23:35:51 +0300

    Page Object first test

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,20 @@
 import pytest, time, os
+from pages.main_page import MainPage
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+#def go_to_login_page(browser):
+#    login_link = browser.find_element_by_css_selector("#login_link")
+#    login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    
+    #browser.get(link)
     #login_link = browser.find_element_by_css_selector("#login_link")
     #login_link.click()
-    go_to_login_page(browser)
+    #go_to_login_page(browser)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    
+    
     
