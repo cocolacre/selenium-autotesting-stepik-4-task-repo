ce3f1bf7b2b00e9a0c408c60b8c4ce09d2ba420e
cocolacre <cocolacre@gmail.com>
2021-03-22 00:25:27 +0300

    navigate through pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
+from .login_page import LoginPage
 from .locators import MainPageLocators
 
 class MainPage(BasePage):
@@ -9,7 +10,8 @@ class MainPage(BasePage):
         #login_link = self.browser.find_element(By.CSS_SELECTOR,"#login_link")
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
-    
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
+        
     def should_be_login_link(self):
         #self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
         #assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link not found!"
